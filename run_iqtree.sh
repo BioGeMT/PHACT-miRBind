9a0d5015c28e64f49fbde6ba851cc8ac5500b163
Dimos Tzimotoudis <158493651+dimostzim@users.noreply.github.com>
2025-03-28 15:14:16 +0100

    lower cpus run_iqtree.sh

--- run_iqtree.sh
+++ run_iqtree.sh
@@ -2,7 +2,7 @@ python evo_scripts/find_alignments.py --input output/annotated_precursors_aligne
 
 python evo_scripts/filter_alignments.py output/all_alignments/ output/all_alignments_filtered/ --min 10
 
-find output/all_alignments_filtered -name "*.aln" | xargs -P 64 -I {} bash -c '
+find output/all_alignments_filtered -name "*.aln" | xargs -P 8 -I {} bash -c '
   aln_file={}
   basename=$(basename "$aln_file" .aln)
   mkdir -p "output/iqtree_out/$basename"
